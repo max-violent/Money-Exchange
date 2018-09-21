a9803db3f4803f632c87ffcad4ab44244d6c6ecb
max-violent <max.violent@gmail.com>
2018-09-21 14:53:15 +0300

    a

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+	money={};
+	cash={'H':50,'Q':25,'D':10,'N':5,'P':1};
+	if (currency>10000){
+		money["error"]='You are rich, my friend! We don\'t have so much coins for exchange';
+}
+	else {
+		for (var i in cash){
+		if (currency>0){
+			var cel=parseInt(currency/cash[i]);
+			if (cel>0){
+				money[i]=cel;
+}
+			currency=currency%cash[i];
+}
+}
+}
+	return(money);
 }
